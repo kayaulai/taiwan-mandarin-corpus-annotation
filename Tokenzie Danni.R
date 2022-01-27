574ad34995b4383e0a14f9642979e4716f770999
danni0312 <97770286+danni0312@users.noreply.github.com>
2022-01-27 11:08:29 -0800

    Update Tokenzie Danni.R

--- Tokenzie Danni.R
+++ Tokenzie Danni.R
@@ -1,8 +1,9 @@
 library(spacyr)
+library(tidyverse)
 
 spacy_install(lang_models="zh_core_web_sm")
 spacy_initialize(model="zh_core_web_sm")
-data<-read_csv("~/Desktop/NCCU-TM026-CN-MM (Sample data)  - Transcribing (2).csv")
+data<-read_csv("~/Desktop/modifieddata.csv")
 unlist(data$Utterance)
 parsefile <- str_replace_all(data$Utterance,"[[:print:]]","")
 parsedfile<-spacy_tokenize(data$Utterance)
