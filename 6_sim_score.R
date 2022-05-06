b8c8ccf2198157802293f9dd92213918d0d180c6
JayyyyLee <97768904+JayyyyLee@users.noreply.github.com>
2022-05-06 11:20:46 -0700

    Update 6_sim_score.R

--- 6_sim_score.R
+++ 6_sim_score.R
@@ -3,16 +3,16 @@ library(readr)
 library(reshape)
 library(tidyverse)
 library(dplyr)
+## only need to change file name in line 406 407
 
-"{ }{,}{,}{ }{.}{ }{ }{,}{.}{ }{-}{,}{ }{,}"
-"{ }{ }{,}{-}{!}{,}{ }{ }{.}{,}{ }{ }{-}{,}"
 
-# Replace --
+# Replace -- to +
+# can only deal with single notes
 reDS <- function(d){
   result=mutate(d, Utterance = (str_replace_all(d$Utterance, "--", '+')))
   return(result)
-}
-# Replace NA speakers
+} # input raw dataframe
+# Replace NA speakers with the speaker above
 reNA <- function(d){
   for (i in seq(1,length(d$Utterance))){  # for each row
     if (is.na(d$Speaker[i])){
@@ -20,12 +20,12 @@ reNA <- function(d){
     }
   }
   return(d)
-}  # input modified data from csv
-# Seperate speakers
+}  # input reDS 
+# Seperate speakers into a dataframe
 sepSpeaker <- function(d){
-  num_speaker=length(levels(as.factor(d$Speaker)))  
+  num_speaker=length(levels(as.factor(d$Speaker)))  # numbder of speakers
   # Create empty datadrame
-  sep=data.frame(array(dim = c(max(summary(as.factor(d$Speaker))),1,2)))
+  sep=data.frame(array(dim = c(max(summary(as.factor(d$Speaker))),1,2))) # seperation result
   colnames(sep)=levels(as.factor(d$Speaker))
   # Seperate
   for (s in seq(1,num_speaker)){  # for each speaker
@@ -40,8 +40,8 @@ sepSpeaker <- function(d){
 # Generate boundry lists
 # return one list of boundary 
 genBd <- function(d,sep){
-  num_speaker=length(levels(as.factor(d$Speaker)))
-  bd=data.frame(array(c('',''),dim = c(1,1,num_speaker)))
+  num_speaker=length(levels(as.factor(d$Speaker))) # number of speakers
+  bd=data.frame(array(c('',''),dim = c(1,1,num_speaker)))  # result boundry list
   colnames(bd)=levels(as.factor(d$Speaker))
   # fill in boundry lists
   for (s in seq(1,num_speaker)){  # for each speaker
@@ -64,9 +64,9 @@ genBd <- function(d,sep){
 # Calculate cost of two boundary lists
 # sub=1 cost, trans=0.5 cost/dist 
 calCost <- function(l1,l2){
-  record=data.frame()
-  subCost=1
-  transCost=0.5
+  record=data.frame()  # result dataframe of record process
+  subCost=1  # pre-set substitution cost
+  transCost=0.5  # pre-set transition cost of one space
   if (length(l1)!=length(l2)){
     return ("different speakers try again")
   }  # check speaker num
@@ -76,20 +76,20 @@ calCost <- function(l1,l2){
     }
   }  # check element length
   
-  cost=0  
+  cost=0  # initialize the total cost
   t1=""
   t2=""  # temp for cost between two fixed boundries
   for (s in seq(1,length(l1))){  # for each speacker
     for (i in seq(1,nchar(l1[s]))){  # for index of element in each list
-      e1 =substring(l1[s],i,i)
-      e2 =substring(l2[s],i,i)
+      e1 =substring(l1[s],i,i)  # ith element in t1
+      e2 =substring(l2[s],i,i)  # ith element in t2
       if (e1==e2 & e1!=" " & e2!=" "){
         if (t1!="" & t2!="" ){
-          t11=data.frame(n=seq(nchar(t1)),e=c(sepChar(t1)[[1]]))
-          t22=data.frame(n=seq(nchar(t2)),e=c(sepChar(t2)[[1]]))
-          paresult=parSim(t11,t22)
-          sd=sTOd(paresult[2])
-          cost=cost+as.numeric(paresult[1])
+          t11=data.frame(n=seq(nchar(t1)),e=c(sepChar(t1)[[1]]))  
+          t22=data.frame(n=seq(nchar(t2)),e=c(sepChar(t2)[[1]]))  # formated dataframe of t1 t2 and length list
+          paresult=parSim(t11,t22)  # result of cost and record between two fixed boundries
+          sd=sTOd(paresult[2])  # formated process of change
+          cost=cost+as.numeric(paresult[1])  
           if (paresult[2] !=""){
             df=data.frame(speaker=s,
                           type=sd$type,
@@ -158,21 +158,22 @@ calCost <- function(l1,l2){
   }
   return(c(cost,record))
 }  # input 2 genBd
+# With record
 # Calculate cost between each sub string Without record
 parSim <- function(t1,t2){
   subCost=1
   transCost=0.5
   
-  e1 =t1$e[1]
-  e2 =t2$e[1]
+  e1 =t1$e[1] # first element of t1
+  e2 =t2$e[1] # first element of t2
   
-  s1=t1[2:nrow(t1),]
+  s1=t1[2:nrow(t1),]  # sublist without first element
   s2=t2[2:nrow(t2),]
   
-  t=rbind(t2[2,],t2[1,],t2[3:nrow(t1),])
+  t=rbind(t2[2,],t2[1,],t2[3:nrow(t1),])  # switch list of first two element
   
-  sdf=paste0("s","#",t1$n[1],"#",e1,"#",t2$n[1],"#",e2,"&")
-  tdf=paste0("t","#",t1$n[1],"#",e1,"#",t1$n[2],"#",t1$e[2],"#",t2$n[1],"#",e2,"#",t2$n[2],"#",t2$e[2],"&")
+  sdf=paste0("s","#",t1$n[1],"#",e1,"#",t2$n[1],"#",e2,"&")  # substitution process moduel 
+  tdf=paste0("t","#",t1$n[1],"#",e1,"#",t1$n[2],"#",t1$e[2],"#",t2$n[1],"#",e2,"#",t2$n[2],"#",t2$e[2],"&") # substitution process moduel 
 
   
   if (nrow(t1)<=1){
@@ -222,7 +223,7 @@ parSim <- function(t1,t2){
       }
     }
   }
-}
+}  # input two sub boundry list
 # Without record
 # Calculate cost of two boundary lists
 calCost1<-function(l1,l2){
@@ -271,6 +272,7 @@ calCost1<-function(l1,l2){
   }
   return(cost)
 }  # input 2 genBd
+# Without record
 # Calulate cost between each sub string Without record
 parSim1<- function(t1,t2){
   subCost=1
@@ -302,14 +304,14 @@ parSim1<- function(t1,t2){
       }
     }
   }
-}
+}  # input two sub boundry list
 # Transform the parsim String to dataframe.
 sTOd <- function(s){
-  slist=strsplit(s,"&")
-  result= data.frame()
+  slist=strsplit(s,"&")  # split the process string by & into steps
+  result= data.frame()  # result dataframe of process
   for (i in slist[[1]]){
-    slist2=strsplit(i,"#")
-    a=slist2[[1]][1]
+    slist2=strsplit(i,"#")  # split the process string by # into 5 elements: type positions charactors
+    a=slist2[[1]][1]  # type of the step :s or t
     if (a=="s"){
       result=rbind(result, data.frame(type="s",
                               oldPosition=slist2[[1]][2], 
@@ -334,15 +336,15 @@ sTOd <- function(s){
     }
   }
   return(result)
-}
+}  # input boundry cost result
 # Separate strings
 sepChar <- function(t1){
   return (strsplit(t1,''))
 }
 # Calculate bound number
 bdNum <- function(bd){  # input boundry list
-  n=0
-  l=0
+  n=0  # number of boundries in a list
+  l=0  # temperate variable for counting
   for (s in length(bd)){  # for each speacker
     for (i in seq(1,nchar(bd[s]))){  # for index of element in each list
       e =substring(bd[1,s],i,i)
