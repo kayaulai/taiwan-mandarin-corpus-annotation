06e6dcb2e36ea535defa667fc296623923c7c7e5
Ryan Ka Yau Lai <43101723+kayaulai@users.noreply.github.com>
2022-06-03 16:16:41 -0700

    Remove Yujie's draft, add TM049 Rez file

--- 8_to_rez.R
+++ 8_to_rez.R
@@ -1,15 +1,16 @@
-filename = "NCCU-TM026-CN-MM_dft.csv"
-data = read_csv(paste0("6_dft_converted/", filename))[,-c(1,2,6)]
+filename = "NCCU-TM049.csv"
+setwd("C:/Users/User/Documents/GitHub/taiwan-mandarin-corpus-annotation")
+data = read_csv(paste0("7_dft_converted/", filename))[,]
 
 data_owpl = data.frame(TurnSeq = integer(0), IUSeq = integer(0), Speaker = character(0), Utterance = character(0))
 splits = strsplit(as.character(data$Utterance)," ")
 for(i in 1:nrow(data)){
   data_owpl = add_row(data_owpl,
                       TurnSeq = pull(data[i,],"TurnSeq"),
-                      IUSeq = pull(data[i,4],"IUSeq"),
+                      IUSeq = pull(data[i,],"IUSeq"),
                       Speaker = pull(data[i,],"Speaker"),
                       Utterance = splits[[i]])
 }
 filename = substr(filename, 1, (nchar(filename)-3))
 data_owpl = cbind(filename,data_owpl)
-write_csv(data_owpl, paste0("7_rez_input/", substr(filename, 1, (nchar(filename)-5)), ".csv"))
+write_csv(data_owpl, paste0("8_rez_input/", "NCCU-TM049.csv"))
