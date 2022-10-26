0010cf23b9b85d3488fd185fd463bc1d73f6d652
Ryan Ka Yau Lai <43101723+kayaulai@users.noreply.github.com>
2022-10-26 15:48:34 -0700

    Installations file

---
+++ 0_installations.R
@@ -0,0 +1,6 @@
+install.packages(c("tidyverse", "spacyr"))
+
+#You should install Anaconda before these steps: https://www.anaconda.com/
+library(spacyr)
+spacy_install(lang_models="zh_core_web_sm")
+#You will be asked if you want to proceed. Type '2' to proceed.
