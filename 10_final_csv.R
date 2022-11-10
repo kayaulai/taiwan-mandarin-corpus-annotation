38ad6753451d3c6fca78f945b3b27620ae88149d
Ryan Ka Yau Lai <43101723+kayaulai@users.noreply.github.com>
2022-11-10 15:25:23 -0800

    Fix CSV, add Rezonator section in PPT

--- 10_final_csv.R
+++ 10_final_csv.R
@@ -1,8 +1,9 @@
 library(rezonateR)
 
+setwd("C:/Users/User/Documents/GitHub/taiwan-mandarin-corpus-annotation")
 docname = "NCCU-TM025-CN-FM.rez"
 finalRez = importRez("9_final_split/" %+% docname, concatFields = "Utterance")
-finalRez$tokenDF = finalRez$tokenDF %>% rez_mutate(Utterance = case_when(str_detect(Utterance, "\\[{0,1}\\.\\.\\]{0,1}") ~ "(.)", T ~ Utterance))
+finalRez$tokenDF = finalRez$tokenDF %>% rez_mutate(Utterance = case_when(str_detect(Utterance, "\\[{0,1}\\.\\.\\]{0,1}") & !str_detect(Utterance, "\\.\\.\\.") ~ "(.)", T ~ Utterance))
 finalRez$entryDF = reload(finalRez$entryDF, finalRez)
 finalRez$unitDF = reload(finalRez$unitDF, finalRez)
 
