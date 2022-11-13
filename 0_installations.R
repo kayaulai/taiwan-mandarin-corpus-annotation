1ca2a9340d62c9672727b8e1a3aeef4bd2e6d1c1
Ryan Ka Yau Lai <43101723+kayaulai@users.noreply.github.com>
2022-11-12 22:14:29 -0800

    Some minor text corrections; add rezonateR to installations

--- 0_installations.R
+++ 0_installations.R
@@ -4,3 +4,7 @@ install.packages(c("tidyverse", "spacyr"))
 library(spacyr)
 spacy_install(lang_models="zh_core_web_sm")
 #You will be asked if you want to proceed. Type '2' to proceed.
+
+install.packages("devtools")
+library(devtools)
+install_github("rezonators/rezonateR")
