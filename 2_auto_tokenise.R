58daeec561e8e5ba6ce6c3c2c474e1df989b00a2
Ryan Ka Yau Lai <43101723+kayaulai@users.noreply.github.com>
2023-01-13 02:53:02 -0800

    Updates to tokenisation code

--- 2_auto_tokenise.R
+++ 2_auto_tokenise.R
@@ -4,45 +4,45 @@ library("stringr")
 library(dplyr)
 spacy_initialize(model="zh_core_web_sm")
 
-file_name <- "NCCU-TM004-CN-FF.csv"  # Only change the file path to the input file in <1_auto_split>
+file_name <- "NCCU-TM013-CN-FFF.csv"  # Only change the file path to the input file in <1_auto_split>
 data <- read_csv(paste0("1_auto_split/", file_name))
 
 unlist(data$Utterance)
 parsedfile <- str_replace_all(data$Utterance,"[[:print:]]","")
 parsedfile<-spacy_tokenize(data$Utterance)
 ## change ??()??
-changeB<-function(p){
-  for (i in seq(1,length(p))){ #Loop on lines
-    temp=""
-    temp2=""
-    x=0
-    for (g in seq(1,length(p[[i]]))){ #Loop on tokens
-      print(p[[i]][g])
-      if (substring(p[[i]][g],1,1)=="("){ #Token starts with a (
-        for (j in seq(1,length(p[[i]]))){
-          for (k in seq(1,nchar(p[[i]][j]))){
-            e=substring(p[[i]][j],k,k)
-            temp=paste0(temp,e)
-            if (e==")"){
-              x=1
-              temp2=substring(p[[i]][j],k+1,nchar(p[[i]][j]))
-              z=j+1
-              temp3=p[[i]][z:length(p[[i]])]
-              break
-            }
-          } 
-          if (x==1){
-            break
-            x=0
-          }
-        }
-        p[[i]]=c(temp,temp2,temp3)
-      }
-  }
-  }
-  return(p)
-}
-parsedfile<-changeB(parsedfile)
+# changeB<-function(p){
+#   for (i in seq(1,length(p))){ #Loop on lines
+#     temp=""
+#     temp2=""
+#     x=0
+#     for (g in seq(1,length(p[[i]]))){ #Loop on tokens
+#       print(p[[i]][g])
+#       if (substring(p[[i]][g],1,1)=="("){ #Token starts with a (
+#         for (j in seq(1,length(p[[i]]))){
+#           for (k in seq(1,nchar(p[[i]][j]))){
+#             e=substring(p[[i]][j],k,k)
+#             temp=paste0(temp,e)
+#             if (e==")"){
+#               x=1
+#               temp2=substring(p[[i]][j],k+1,nchar(p[[i]][j]))
+#               z=j+1
+#               temp3=p[[i]][z:length(p[[i]])]
+#               break
+#             }
+#           }
+#           if (x==1){
+#             break
+#             x=0
+#           }
+#         }
+#         p[[i]]=c(temp,temp2,temp3)
+#       }
+#   }
+#   }
+#   return(p)
+# }
+# parsedfile<-changeB(parsedfile)
 parsedfile
 conjunct<-function(p){
   for (i in seq(1,length(p))){
@@ -53,7 +53,7 @@ conjunct<-function(p){
       new_p = paste(a,collapse = " ")
     }
     p[[i]] = new_p
-    
+
     #Non-Mandarin language tags and tokens
     p[[i]]=str_replace_all(p[[i]],"< L","<L")
     p[[i]]=str_replace_all(p[[i]],"(\\d) >","\\1>")
@@ -61,7 +61,7 @@ conjunct<-function(p){
     p[[i]]=str_replace_all(p[[i]],"([^ <])L(\\d)","\\1 L\\2")
     p[[i]]=str_replace_all(p[[i]],"L(\\d)([^ >])","L\\1 \\2")
     p[[i]]=str_replace_all(p[[i]],"([A-Za-z]) \\d ([A-Za-z])","\\1\\d \\2") #Putting tone and syllable together
-    
+
     #Laughter tags
     p[[i]]=str_replace_all(p[[i]],"< @","<@")
     p[[i]]=str_replace_all(p[[i]],"@ >","@>")
@@ -77,30 +77,54 @@ conjunct<-function(p){
     p[[i]]=str_replace_all(p[[i]],"@ \\[\\[@","@[[@")
     p[[i]]=str_replace_all(p[[i]],"@([^ @])","@ \\1")
     p[[i]]=str_replace_all(p[[i]],"([^ @])@","\\1 @")
-    
+    p[[i]]=str_replace_all(p[[i]],"<@([^ ])", "<@ \\1")
+    p[[i]]=str_replace_all(p[[i]],"([^ ])@>", "\\1 @>")
+
+    #Other tags
+    p[[i]]=str_replace_all(p[[i]],"< L", "<L")
+    p[[i]]=str_replace_all(p[[i]],"(\\d) >","\\1>")
+    p[[i]]=str_replace_all(p[[i]],"< X", "<X")
+    p[[i]]=str_replace_all(p[[i]],"X >","X>")
+
+
+    p[[i]]=str_replace_all(p[[i]],"<X([^ ])", "<X \\1")
+    p[[i]]=str_replace_all(p[[i]],"([^ ])X>", "\\1 X>")
+    p[[i]]=str_replace_all(p[[i]],"<L(\\d)([^ ])", "<L\\1 \\2")
+    p[[i]]=str_replace_all(p[[i]],"([^ ])L(\\d)>", "\\1 L\\2>")
+
+
+    p[[i]]=str_replace_all(p[[i]],"X X","XX")
+
     #Brackets: Removing spaces between brackets and content
     p[[i]]=str_replace_all(p[[i]],"\\( ","(")
     p[[i]]=str_replace_all(p[[i]]," \\)",")")
     p[[i]]=str_replace_all(p[[i]],"\\[ ","[") # change [ a ] to [a]
     p[[i]]=str_replace_all(p[[i]]," \\]" ,"]")
-    
+    p[[i]]=str_replace_all(p[[i]],"\\( \\(","((")
+    p[[i]]=str_replace_all(p[[i]],"\\) \\)","))")
+
     #Brackets; Adding spaces between brackets and non-content
     p[[i]]=str_replace_all(p[[i]],"([^ ])<" ,"\\1 <")#change <to<
     p[[i]]=str_replace_all(p[[i]],">([^ ])","> \\1") #first digit after >
-    p[[i]]=str_replace_all(p[[i]],"([^ ])\\(","\\1 \\(") #change )to) 
-    p[[i]]=str_replace_all(p[[i]],"\\)([^ ])","\\) \\1") #change )to) 
-    
+    p[[i]]=str_replace_all(p[[i]],"([^ ])\\(","\\1 \\(") #change )to)
+    p[[i]]=str_replace_all(p[[i]],"\\)([^ ])","\\) \\1") #change )to)
+
     #Pause-specific
+    p[[i]]=str_replace_all(p[[i]],"\\( (\\d)","(\\1")
+    p[[i]]=str_replace_all(p[[i]],"(\\d) \\)","\\1)")
     p[[i]]=str_replace_all(p[[i]],"(\\d) \\.","\\1.")
     p[[i]]=str_replace_all(p[[i]],"\\. (\\d)"  ,".\\1")
     p[[i]]=str_replace_all(p[[i]],"\\. \\.","\\.\\.")
     p[[i]]=str_replace_all(p[[i]],"\\.([^ .])","\\. \\1")
-    
-    #Lengthening
+
+    #Lengthening and truncation
     p[[i]]=str_replace_all(p[[i]]," =","=")
     p[[i]]=str_replace_all(p[[i]]," \\[=","[=")
     p[[i]]=str_replace_all(p[[i]]," \\[\\[=","[[=")
+    p[[i]]=str_replace_all(p[[i]]," -","-")
 
+    #Bonus
+    p[[i]]=str_replace_all(p[[i]],"覺 得","覺得")
   }
   return(p)
 }
